ec56453f18016b69dd7d5a97c03c0bda7ecaa02f
john.reyes <john.reyes@ias.com.co>
2021-12-06 13:34:27 -0500

    stream flux observable

--- spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,22 @@
 package com.bolsadeideas.springboot.reactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Andres", "Pedro", "Diego", "Juan")
+				.doOnNext(elemento -> System.out.println(elemento));
+		nombres.subscribe();
+	}
 }
