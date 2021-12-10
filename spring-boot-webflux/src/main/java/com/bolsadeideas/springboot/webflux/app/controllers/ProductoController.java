12eeb365f5aaa21a6d8e76bb6bba283015805428
john.reyes <john.reyes@ias.com.co>
2021-12-10 14:01:45 -0500

    add chunked mode view names

--- spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -55,9 +55,21 @@ public class ProductoController {
 				.map(producto -> {
 					producto.setNombre(producto.getNombre().toUpperCase());
 					return producto;
-				}).repeat(100);
+				}).repeat(5000);
 		model.addAttribute("productos", productos);
 		model.addAttribute("titulo", "Listado de productos");
 		return "listar";
+	}
+	
+	@GetMapping("/listar-chunked")
+	public String listarChunked(Model model) {
+		Flux<Producto> productos = productoDao.findAll()
+				.map(producto -> {
+					producto.setNombre(producto.getNombre().toUpperCase());
+					return producto;
+				}).repeat(5000);
+		model.addAttribute("productos", productos);
+		model.addAttribute("titulo", "Listado de productos");
+		return "listar-chunked";
 	}
 }
