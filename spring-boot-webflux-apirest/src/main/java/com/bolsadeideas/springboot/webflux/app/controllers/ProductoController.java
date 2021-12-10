8df5480a31f46f12e8cd5abbfadc6d14c0c602cb
john.reyes <john.reyes@ias.com.co>
2021-12-10 16:43:16 -0500

    add handler get to list products with restController

---
+++ spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -0,0 +1,29 @@
+package com.bolsadeideas.springboot.webflux.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+import com.bolsadeideas.springboot.webflux.app.models.services.ProductoService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoController {
+
+	@Autowired
+	private ProductoService service;
+	
+	@GetMapping
+	public Mono<ResponseEntity<Flux<Producto>>> lista() {
+		return Mono.just(ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(service.findAll()));
+	}
+}
