d626d88f60e4423af220ce3be89fb9f43566b467
john.reyes <john.reyes@ias.com.co>
2021-12-10 13:13:03 -0500

    subscribe another observable and modify reactive stream

--- spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.webflux.app.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,9 +18,16 @@ public class ProductoController {
 	@Autowired
 	private IProductoDao productoDao;
 	
+	private static final Logger logger = LoggerFactory.getLogger(ProductoController.class);
+	
 	@GetMapping({"/", "/listar"})
 	public String listar(Model model) {
-		Flux<Producto> productos = productoDao.findAll();
+		Flux<Producto> productos = productoDao.findAll()
+				.map(producto -> {
+					producto.setNombre(producto.getNombre().toUpperCase());
+					return producto;
+				});
+		productos.subscribe(producto -> logger.info(producto.getNombre()));
 		model.addAttribute("productos", productos);
 		model.addAttribute("titulo", "Listado de productos");
 		return "listar";
