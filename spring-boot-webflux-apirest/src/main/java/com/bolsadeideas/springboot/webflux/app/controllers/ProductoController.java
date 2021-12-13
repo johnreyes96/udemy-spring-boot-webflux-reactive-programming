cbb1b6e894e76bd29accefaf6dda822373936ca4
john.reyes <john.reyes@ias.com.co>
2021-12-13 08:39:33 -0500

    post new product apirest

--- spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -1,10 +1,15 @@
 package com.bolsadeideas.springboot.webflux.app.controllers;
 
+import java.net.URI;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +39,17 @@ public class ProductoController {
 				.contentType(MediaType.APPLICATION_JSON)
 				.body(producto))
 				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public Mono<ResponseEntity<Producto>> crear(@RequestBody Producto producto) {
+		if (producto.getCreateAt() == null) {
+			producto.setCreateAt(new Date());
+		}
+		
+		return service.save(producto).map(product -> ResponseEntity
+				.created(URI.create("/api/productos/".concat(product.getId())))
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(product));
 	}
 }
