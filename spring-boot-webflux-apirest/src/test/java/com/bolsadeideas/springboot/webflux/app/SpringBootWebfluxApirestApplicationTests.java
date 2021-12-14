5c3ccc243e63ce89b669dece933c9f4fc56b2c3a
john.reyes <john.reyes@ias.com.co>
2021-12-14 09:48:11 -0500

    add unit test to listar endpoint

--- spring-boot-webflux-apirest/src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ spring-boot-webflux-apirest/src/test/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,32 @@
 package com.bolsadeideas.springboot.webflux.app;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
+	
+	@Autowired
+	private WebTestClient client;
 
 	@Test
-	void contextLoads() {
+	public void listarTest() {
+		
+		client.get()
+		.uri("/api/v2/productos")
+		.accept(MediaType.APPLICATION_JSON)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON)
+		.expectBodyList(Producto.class)
+		.hasSize(9);
 	}
-
 }
