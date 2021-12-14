79ed78c274a6bcec8ec3a6a9d1175c9b4b717a0d
john.reyes <john.reyes@ias.com.co>
2021-12-14 14:10:00 -0500

    add post, put and delete service

--- spring-boot-webflux-client/src/main/java/com/bolsadeideas/springboot/webflux/client/app/models/services/ProductoServiceImpl.java
+++ spring-boot-webflux-client/src/main/java/com/bolsadeideas/springboot/webflux/client/app/models/services/ProductoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.webflux.client.app.models.services;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,6 @@ import reactor.core.publisher.Mono;
 
 import com.bolsadeideas.springboot.webflux.client.app.models.Producto;
 
-import static org.springframework.web.reactive.function.BodyInserters.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;;
 
 public class ProductoServiceImpl implements IProductoService {
@@ -41,18 +41,27 @@ public class ProductoServiceImpl implements IProductoService {
 		return client.post()
 				.contentType(APPLICATION_JSON)
 				.accept(APPLICATION_JSON)
-				.body(fromValue(producto))
+				.bodyValue(producto)
 				.retrieve()
 				.bodyToMono(Producto.class);
 	}
 
 	@Override
 	public Mono<Producto> update(Producto producto, String id) {
-		return null;
+		return client.put()
+				.uri("/{id}", Collections.singletonMap("id", id))
+				.contentType(APPLICATION_JSON)
+				.accept(APPLICATION_JSON)
+				.bodyValue(producto)
+				.retrieve()
+				.bodyToMono(Producto.class);
 	}
 
 	@Override
 	public Mono<Void> delete(String id) {
-		return null;
+		return client.delete()
+				.uri("/{id}", Collections.singletonMap("id", id))
+				.retrieve()
+				.bodyToMono(Void.class);
 	}
 }
