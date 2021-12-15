292577a1fd8c6e8c5446ce479af2ae554a486c98
john.reyes <john.reyes@ias.com.co>
2021-12-15 10:44:33 -0500

    create registration server in Eureka

--- spring-boot-webflux-eureka-server/src/main/java/com/bolsadeideas/springboot/webflux/eureka/app/SpringBootWebfluxEurekaServerApplication.java
+++ spring-boot-webflux-eureka-server/src/main/java/com/bolsadeideas/springboot/webflux/eureka/app/SpringBootWebfluxEurekaServerApplication.java
@@ -2,12 +2,13 @@ package com.bolsadeideas.springboot.webflux.eureka.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringBootWebfluxEurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebfluxEurekaServerApplication.class, args);
 	}
-
 }
