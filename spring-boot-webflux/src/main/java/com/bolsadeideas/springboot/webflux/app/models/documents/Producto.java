a9b10a8dc7722efb91ea56038c9afa61a29db39d
john.reyes <john.reyes@ias.com.co>
2021-12-15 14:23:27 -0500

    add form view

--- spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/models/documents/Producto.java
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/models/documents/Producto.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Document(collection = "productos")
 public class Producto {
@@ -13,6 +14,8 @@ public class Producto {
 	
 	private String nombre;
 	private Double precio;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 	
 	public Producto() { }
