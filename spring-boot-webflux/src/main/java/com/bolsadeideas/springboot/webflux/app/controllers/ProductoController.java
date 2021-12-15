2bda453ca1091dbd8eef5bef63995a6ae4a5100e
john.reyes <john.reyes@ias.com.co>
2021-12-15 16:47:17 -0500

    add model attribute in guardar method when name attribute is different to class name to validate

--- spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -82,7 +83,7 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/form")
-	public Mono<String> guardar(@Valid Producto producto, BindingResult result, Model model, SessionStatus status) {
+	public Mono<String> guardar(@Valid @ModelAttribute("producto") Producto producto, BindingResult result, Model model, SessionStatus status) {
 		//TODO: Don't run the .hasErrors
 		if (result.hasErrors()) {
 			model.addAttribute("titulo", "Errores en formulario producto");
